621d7bfdcbccaf77b7ab6f5bc3fd8f42b45f44ca
Shodhan Shetty <79359457+shodhanshetty14@users.noreply.github.com>
2022-04-19 14:00:21 +0530

    Add files via upload

---
+++ WeightnHeight.py
@@ -0,0 +1,22 @@
+def kg_to_lbs(data):
+    return data*2.2
+
+
+def lbs_to_kg(data):
+    return data/2.2
+
+
+def ft_to_cm(data):
+    return (data*100)/3.28
+
+
+def cm_to_ft(data):
+    return (data*3.8)/100
+
+
+def mile_to_km(data):
+    return data*1.61
+
+
+def km_to_mile(data):
+    return data/1.61
---
+++ temperature_conv.py
@@ -0,0 +1,14 @@
+def celsius_to_fahrenheit(data):
+    return (data*9/5)+32
+
+
+def fahrenheit_to_celsius(data):
+    return (data-32)*5/9
+
+
+def celsius_to_kelvin(data):
+    return data+273
+
+
+def kelvin_to_celsius(data):
+    return data-273
